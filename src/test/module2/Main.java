a2b075bf8fb0ee5c8a94c6ffcc78982e6746d9fe
f-feijoo <84591753+f-feijoo@users.noreply.github.com>
2023-05-23 10:45:32 -0300

    Add files via upload

---
+++ src/test/module2/Main.java
@@ -0,0 +1,31 @@
+package test.module2;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        byte variable1 = 5;
+        System.out.println("byte: "+variable1);
+        short variable2 = 10;
+        System.out.println("short: "+variable2);
+        int variable3 = 30;
+        System.out.println("int: "+variable3);
+        long variable4 = 100;
+        System.out.println("long: "+variable4);
+
+        float variable5 = 5.5f;
+        System.out.println("float: "+variable5);
+        double variable6 = 10.5d;
+        System.out.println("double: "+variable6);
+
+        boolean variable7 = false;
+        System.out.println("boolean: "+variable7);
+        boolean variable8 = true;
+        System.out.println("boolean: "+variable8);
+
+        char variable9 = 'a';
+        System.out.println("char: "+variable9);
+        String variable10 = "Lorem ipsum dolor sit amet.....";
+        System.out.println("String: "+variable10);
+    }
+}
